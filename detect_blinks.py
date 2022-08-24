2a50b7f1c4bc00b9551211fb6bf201bde6d399ac
unknown <roshini.konjeti@gmail.com>
2022-08-24 14:12:43 -0700

    sm2

--- detect_blinks.py
+++ detect_blinks.py
@@ -1,4 +1,4 @@
-# USAGE
+# USAGE sai new
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
