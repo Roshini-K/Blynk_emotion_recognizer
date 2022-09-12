28c984d783b1a2914486df4027ba0cf5acb50509
unknown <roshini.konjeti@gmail.com>
2022-09-12 08:17:32 -0700

    add

--- detect_blinks.py
+++ detect_blinks.py
@@ -1,7 +1,7 @@
 # USAGE
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
-
+#sample1
 # import the necessary packages
 import math
 
